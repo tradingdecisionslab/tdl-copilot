9c1a2035e444083ef17b0e1f184fae57d1fbce3b
tradingdecisionslab <tradingdecisionslab@gmail.com>
2026-03-11 16:28:29 -0400

    Fix import statement and format next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
-import { withWhopAppConfig } from "@whop/react/next.config";
 import type { NextConfig } from "next";
+import { withWhopAppConfig } from "@whop/react/next.config";
 
 const nextConfig: NextConfig = {
-	/* config options here */
-	images: {
-		remotePatterns: [{ hostname: "**" }],
-	},
+  images: {
+    remotePatterns: [{ hostname: "**" }],
+  },
 };
 
 export default withWhopAppConfig(nextConfig);
